000cb08198c6e4c5447e7e9bc9eb45dd14d34054
Stephanie Dunning <eggandcheesesandwich@gmail.com>
2016-03-29 16:44:57 -0700

    chkpt33morecrud: completed

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -7,28 +7,52 @@ class PostsController < ApplicationController
     @post = Post.find(params[:id])
   end
 
-#7
   def new
     @post = Post.new
   end
 
 def create
-#9
   @post = Post.new
   @post.title = params[:post][:title]
   @post.body = params[:post][:body]
-#10
+
   if @post.save
-#11
     flash[:notice] = "Post was saved."
     redirect_to @post
   else
-    #12
     flash.now[:alert] = "There was an error saving the post. Please try again."
     render :new
     end
   end
 
   def edit
+    @post = Post.find(params[:id])
   end
+
+  def update
+   @post = Post.find(params[:id])
+   @post.title = params[:post][:title]
+   @post.body = params[:post][:body]
+
+   if @post.save
+     flash[:notice] = "Post was updated."
+     redirect_to @post
+   else
+     flash.now[:alert] = "There was an error saving the post. Please try again."
+     render :edit
+   end
+ end
+
+ def destroy
+    @post = Post.find(params[:id])
+
+    if @post.destroy
+      flash[:notice] = "\"#{@post.title}\" was deleted successfully."
+      redirect_to posts_path
+    else
+      flash.now[:alert] = "There was an error deleting the post."
+      render :show
+    end
+  end
+
 end
