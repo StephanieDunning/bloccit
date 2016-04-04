518583dda70b9292d041ebca719e40d1b7312381
Stephanie Dunning <eggandcheesesandwich@gmail.com>
2016-04-04 10:53:50 -0700

    assign31introcrud: completed

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -4,6 +4,7 @@ class PostsController < ApplicationController
     @posts.each_with_index do |post, index|
       if index % 5 == 0
         post.title = "SPAM"
+        post.save
       end
     end
   end
