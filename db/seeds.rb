8e3ebe6b5203dd98853293db9f77d1609989db06
Stephanie Dunning <eggandcheesesandwich@gmail.com>
2016-04-04 22:18:12 -0700

    Update seeds.rb

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,29 +1,29 @@
-require 'random_data'
+include RandomData
 
-# Create Posts
 50.times do
   Post.create!(
-  title: RandomData.random_sentence,
-  body: RandomData.random_paragraph
+    title: RandomData.random_sentence,
+    body: RandomData.random_paragraph
   )
 end
 posts = Post.all
+
+50.times do
+  Advertisement.create!(
+    title: RandomData.random_sentence,
+    copy: RandomData.random_paragraph,
+    price: RandomData.random_integer
+  )
+end
 
 100.times do
   Comment.create!(
     post: posts.sample,
     body: RandomData.random_paragraph
-    )
-  end
+  )
+end
 
-100.times do
-  Question.create!(
-    title: RandomData.random_sentence,
-    body: RandomData.random_paragraph,
-    resolved: false
-    )
-  end
-
-  puts "Seed finished"
-  puts "#{Post.count} posts created"
-  puts "#{Comment.count} comments created"
+puts "Seed finished"
+puts "#{Post.count} posts created"
+puts "#{Comment.count} comments created"
+puts "#{Advertisement.count} comments created"
