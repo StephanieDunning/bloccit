5d4db10a4344b7caf178b5fdb2af154df5733d43
Stephanie Dunning <eggandcheesesandwich@gmail.com>
2016-04-06 16:16:23 -0700

    assign35validation: completed

--- spec/models/post_spec.rb
+++ spec/models/post_spec.rb
@@ -1,9 +1,9 @@
 require 'rails_helper'
+include RandomData
 
 RSpec.describe Post, type: :model do
 
   let(:topic) { Topic.create!(name: RandomData.random_sentence, description: RandomData.random_paragraph) }
-
   let(:post) { topic.posts.create!(title: RandomData.random_sentence, body: RandomData.random_paragraph) }
 
   it { is_expected.to belong_to(:topic) }
@@ -13,7 +13,7 @@ RSpec.describe Post, type: :model do
   it { is_expected.to validate_presence_of(:topic) }
 
   it { is_expected.to validate_length_of(:title).is_at_least(5) }
-    it { is_expected.to validate_length_of(:body).is_at_least(20) }
+  it { is_expected.to validate_length_of(:body).is_at_least(20) }
 
   describe "attributes" do
 
