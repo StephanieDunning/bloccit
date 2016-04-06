c6324cbd56ba2d158dc613c6b41ac7056fbf4258
Stephanie Dunning <eggandcheesesandwich@gmail.com>
2016-04-06 14:02:33 -0700

    hkpt34validations: completed

--- spec/models/post_spec.rb
+++ spec/models/post_spec.rb
@@ -8,12 +8,19 @@ RSpec.describe Post, type: :model do
 
   it { is_expected.to belong_to(:topic) }
 
+  it { is_expected.to validate_presence_of(:title) }
+  it { is_expected.to validate_presence_of(:body) }
+  it { is_expected.to validate_presence_of(:topic) }
+
+  it { is_expected.to validate_length_of(:title).is_at_least(5) }
+    it { is_expected.to validate_length_of(:body).is_at_least(20) }
+
   describe "attributes" do
-# #2
+
     it "responds to title" do
       expect(post).to respond_to(:title)
     end
-# #3
+
     it "responds to body" do
       expect(post).to respond_to(:body)
     end
