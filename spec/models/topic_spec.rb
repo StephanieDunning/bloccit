b906c860e27c8a705378f9b8aa62e93f6f8db660
Stephanie Dunning <eggandcheesesandwich@gmail.com>
2016-04-25 09:17:01 -0700

    assign42labels: completed

--- spec/models/topic_spec.rb
+++ spec/models/topic_spec.rb
@@ -6,6 +6,7 @@ RSpec.describe Topic, type: :model do
   it { is_expected.to have_many(:posts) }
   it { is_expected.to have_many(:labelings) }
   it { is_expected.to have_many(:labels).through(:labelings) }
+  it { is_expected.to have_many(:comments) }
 
   describe "attributes" do
     it "responds to name" do
