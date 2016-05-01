49de28fc620e46e50e1c4483db744eae81ac9d6c
Stephanie Dunning <eggandcheesesandwich@gmail.com>
2016-05-01 14:26:55 -0700

    assign47ajax: completed

--- app/controllers/votes_controller.rb
+++ app/controllers/votes_controller.rb
@@ -3,23 +3,29 @@ class VotesController < ApplicationController
 
   def up_vote
     update_vote(1)
-    redirect_to :back
+    respond_to do |format|
+      format.html
+      format.js
+    end
   end
 
   def down_vote
-     update_vote(-1)
-     redirect_to :back
-   end
+    update_vote(-1)
+    respond_to do |format|
+      format.html
+      format.js
+    end
+  end
 
-   private
-   def update_vote(new_value)
-     @post = Post.find(params[:post_id])
-     @vote = @post.votes.where(user_id: current_user.id).first
+  private
+  def update_vote(new_value)
+    @post = Post.find(params[:post_id])
+    @vote = @post.votes.where(user_id: current_user.id).first
 
-     if @vote
-       @vote.update_attribute(:value, new_value)
-     else
-       @vote = current_user.votes.create(value: new_value, post: @post)
-     end
-   end
- end
+    if @vote
+      @vote.update_attribute(:value, new_value)
+    else
+      @vote = current_user.votes.create(value: new_value, post: @post)
+    end
+  end
+end
