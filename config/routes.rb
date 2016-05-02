5a27dadcedf479375275012bc7218444921673bb
Stephanie Dunning <eggandcheesesandwich@gmail.com>
2016-05-02 16:12:44 -0700

    assign48api:partial

--- config/routes.rb
+++ config/routes.rb
@@ -19,4 +19,13 @@ Rails.application.routes.draw do
   get 'about' => 'welcome#about'
 
   root 'welcome#index'
+
+  namespace :api do
+    namespace :v1 do
+      resources :users, only: [:index, :show]
+      resources :topics, only: [:index, :show]
+      resources :posts, only: [:index, :show]
+      resources :comments, only: [:index, :show]
+    end
+  end
 end
