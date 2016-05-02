715b4fa7e9a77ab7586d9d05ed57f3d54766709e
Stephanie Dunning <eggandcheesesandwich@gmail.com>
2016-05-02 12:04:26 -0700

    chkpt48api: completed

--- db/schema.rb
+++ db/schema.rb
@@ -11,15 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160426213943) do
-
-  create_table "advertisements", force: :cascade do |t|
-    t.string   "title"
-    t.text     "copy"
-    t.integer  "price"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-  end
+ActiveRecord::Schema.define(version: 20160502175031) do
 
   create_table "comments", force: :cascade do |t|
     t.text     "body"
@@ -27,11 +19,9 @@ ActiveRecord::Schema.define(version: 20160426213943) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.integer  "user_id"
-    t.integer  "topic_id"
   end
 
   add_index "comments", ["post_id"], name: "index_comments_on_post_id"
-  add_index "comments", ["topic_id"], name: "index_comments_on_topic_id"
   add_index "comments", ["user_id"], name: "index_comments_on_user_id"
 
   create_table "favorites", force: :cascade do |t|
@@ -89,6 +79,7 @@ ActiveRecord::Schema.define(version: 20160426213943) do
     t.datetime "created_at",      null: false
     t.datetime "updated_at",      null: false
     t.integer  "role"
+    t.string   "auth_token"
   end
 
   create_table "votes", force: :cascade do |t|
